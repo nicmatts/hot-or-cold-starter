c936bd3869f039e7ee3bb412950d9845a20489ff
Nicole M <nicmatts@gmail.com>
2014-05-09 15:13:55 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -15,3 +15,36 @@ $(document).ready(function(){
 });
 
 
+
+var newGame = function(){
+	// generate random number
+	var computerNumber = Math.floor(Math.random() * (100 )) + 1;
+	alert(computerNumber);
+
+	//prompt user for guess
+	var userNumber = prompt("Please enter a number between 1 and 100.");
+	userNumber = parseInt(userNumber, 10);
+
+	var checkNum = function(userNumber){
+		if (typeof(userNumber) != 'number'){
+			//prompt("Please enter a valid number between 1 and 100.");
+			alert(typeof(userNumber));
+		} else {
+			alert("number is good.");
+		}
+	};
+
+	checkNum();
+
+	if (userNumber < computerNumber){
+		alert("too low!");
+	} else if (userNumber > computerNumber){
+		alert("too high!");
+	} else if (typeof userNumber != 'number'){
+		prompt("Please enter a valid number between 1 and 100.");
+	} else {
+		alert("you got it!");
+	}
+};
+
+window.onload(newGame());
